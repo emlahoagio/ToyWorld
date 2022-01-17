2c7ed8a3730c0801a3f8f81e39bf5f4da099c856
vinh le <vinhle2311@gmail.com>
2022-01-17 10:08:19 +0700

    edit ToyParameter

--- Contracts/IToyRepository.cs
+++ Contracts/IToyRepository.cs
@@ -10,8 +10,8 @@ namespace Contracts
 {
     public interface IToyRepository
     {
-        Task<Pagination<ToyInList>> GetAllToys(ToyParameters toyParameters, bool trackChanges);
-        Task<Pagination<ToyInList>> GetToysByType(ToyParameters toyParameters, string typeName, bool trackChanges);
+        Task<Pagination<ToyInList>> GetAllToys(PagingParameters toyParameters, bool trackChanges);
+        Task<Pagination<ToyInList>> GetToysByType(PagingParameters toyParameters, string typeName, bool trackChanges);
         Task<ToyDetail> GetToyDetail(int toyId, bool trackChanges);
         void CreateToy(Toy toy);
         void UpdateToy(Toy toy);
