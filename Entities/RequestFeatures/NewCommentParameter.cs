e27ac6805a76146135704d204fa70118352f7b06
vinh le <vinhle2311@gmail.com>
2022-01-19 21:11:40 +0700

    add required parameter for function create new comment

--- Entities/RequestFeatures/NewCommentParameter.cs
+++ Entities/RequestFeatures/NewCommentParameter.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Entities.RequestFeatures
 {
     public class NewCommentParameter
     {
+        [Required]
         public int AccountId { get; set; }
+        [Required]
         public int PostId { get; set; }
+        [Required]
         public string Content { get; set; }
     }
 }
