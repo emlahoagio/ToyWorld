ce7c740e4dd807d8ffbd75f9ccc20e09054c7377
vinh le <vinhle2311@gmail.com>
2022-02-12 15:45:51 +0700

    add prize repository

---
+++ Contracts/Repositories/IPrizeRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contracts.Repositories
+{
+    public interface IPrizeRepository
+    {
+        void CreatePrize(Prize prize);
+        void UpdatePrize(Prize prize);
+    }
+}
