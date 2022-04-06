e0ada27fa2ebc1932796b777e2d87b59112c117c
Do Trong Minh Quan <35829523+emlahoagio@users.noreply.github.com>
2022-04-06 14:44:20 +0700

    edit create contest remove (brand, max-mis regis)

--- Entities/RequestFeatures/CreateContestParameters.cs
+++ Entities/RequestFeatures/CreateContestParameters.cs
@@ -11,13 +11,13 @@ namespace Entities.RequestFeatures
         public string Description { get; set; }
         public string CoverImage { get; set; }
         public string Slogan { get; set; }
-        public int? MinRegistration { get; set; }
-        public int? MaxRegistration { get; set; }
+        //public int? MinRegistration { get; set; }
+        //public int? MaxRegistration { get; set; }
         public DateTime? StartRegistration { get; set; }
         public DateTime? EndRegistration { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-        public string BrandName { get; set; }
+        //public string BrandName { get; set; }
         public string TypeName { get; set; }
     }
 }
---
+++ ToyWorldSystem/Hubs/NotificationHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToyWorldSystem.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        private readonly IDictionary<string, UserConnection> _connections;
+
+        public NotificationHub(IDictionary<string, UserConnection> connections)
+        {
+            _connections = connections;
+        }
+
+
+    }
+}
