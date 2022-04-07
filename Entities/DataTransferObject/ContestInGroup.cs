fdd7565ca8254d96b62ca67de87ffdff2ecd6f72
vinh le <vinhle2311@gmail.com>
2022-04-08 00:33:08 +0700

    remove isjoined in get list contest

--- Entities/DataTransferObject/ContestInGroup.cs
+++ Entities/DataTransferObject/ContestInGroup.cs
@@ -12,7 +12,6 @@ namespace Entities.DataTransferObject
         public string Description { get; set; }
         public int? MinRegistration { get; set; }
         public int? MaxRegistration { get; set; }
-        public bool IsJoined { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
         public DateTime? StartRegistration { get; set; }
