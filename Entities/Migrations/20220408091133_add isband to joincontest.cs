91ceab8c9a10a26984f1074ab932d91a9bcd063d
vinh le <vinhle2311@gmail.com>
2022-04-08 16:14:56 +0700

    update joincontest table

---
+++ Entities/Migrations/20220408091133_add isband to joincontest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entities.Migrations
+{
+    public partial class addisbandtojoincontest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBand",
+                table: "JoinedToContest",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBand",
+                table: "JoinedToContest");
+        }
+    }
+}
