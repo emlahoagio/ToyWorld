a4f8230a3c7415a63c32ab9f5c6e402e6517674e
vinh le <vinhle2311@gmail.com>
2022-05-02 10:24:33 +0700

    create bill

--- Entities/RequestFeatures/NewBillParameters.cs
+++ Entities/RequestFeatures/NewBillParameters.cs
@@ -8,14 +8,8 @@ namespace Entities.RequestFeatures
         [MinLength(6)]
         [MaxLength(256)]
         public string ToyOfSellerName { get; set; }
-        [Required]
-        [MinLength(6)]
-        [MaxLength(256)]
         public string ToyOfBuyerName { get; set; }
-        [Required]
         public bool IsExchangeByMoney { get; set; }
-        [Required]
-        [MaxLength(11)]
         public double? ExchangeValue { get; set; }
         [Required]
         public int BuyerId { get; set; }
